3f069b6940a1df79686b3ec5ed360a6a82354304
Masrakova <masrakovaalesya13@gmail.com>
2023-12-28 16:34:44 +0300

    Изменение фона

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -167,6 +167,7 @@ namespace WindowsFormsApp4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(477, 386);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button7);
